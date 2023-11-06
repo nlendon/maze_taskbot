eba811aec6dd15c61af1ba7e38c2641ad9432c98
nLendon <86872522+nlendon@users.noreply.github.com>
2023-11-06 15:54:13 +0400

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -2,12 +2,11 @@ import { Sequelize } from 'sequelize';
 import 'dotenv/config';
 
 export const Database = new Sequelize(
-    'ar1stokrat_test',
-    'ar1stokrat_test',
+    'ar1stokrat_mbot',
+    'ar1stokrat_mbot',
     'ytHqYuNNml19972941',
     {
         dialect: 'mysql',
-        host: 'ar1stokrat.beget.tech',
-        port: 3306
+        host: 'ar1stokrat.beget.tech'
     }
 );
